c8272921b296ff00fcc893054d33c63219a5ba98
open citations <marilena.daquino2@unibo.it>
2021-11-04 18:19:28 +0100

    wip tuning sections w/ equaliser

--- portal/static/js/main.js
+++ portal/static/js/main.js
@@ -1,45 +1,9 @@
 
+var firstScroll = false;
 
 document.addEventListener("DOMContentLoaded", function(event) {
   // SEARCH RESULTS
   $("#search").hide();
-
-  console.log(sections);
-  // CHANGE HEADER ON SCROLLING
-  var equaliser = document.getElementById("equaliser");
-  $(window).scroll(function(){
-    if (equaliser != null) {
-      // move back to body if scroll top
-      var scrolltop = $(window).scrollTop();
-      if (scrolltop == 0) {
-        var menu_equaliser = $("#equaliser");
-        $("#equaliser").detach();
-        menu_equaliser.appendTo("#equaliser_container");
-        $(".equaliser").css('color','white');
-        $(".equaliser").css('margin-top','5em');
-        $("#header").css('background','black');
-        $("#header").css('height','3rem');
-        $("body, .main_bg").css("background","black");
-      } else {
-
-        $.doTimeout( 'scrollTop', 50, function(){
-          // move equaliser to the menu if scroll down
-           var menu_equaliser = $("#equaliser");
-           $("#equaliser").detach();
-           $("#header").css('background','white');
-           $("#header").css('height','20em');
-           menu_equaliser.appendTo("#header");
-           $("#header .equaliser").css('color','black');
-           $("#header .equaliser").css('margin-top','-5em');
-           $("body, .main_bg").css("background","white");
-
-          // add sections based on equaliser
-
-          });
-      }
-    }
-
-  });
 
   // NAVBAR
   const showNavbar = (toggleId, navId, bodyId, headerId) =>{
@@ -73,11 +37,102 @@ document.addEventListener("DOMContentLoaded", function(event) {
   }
   linkColor.forEach(l=> l.addEventListener('click', colorLink))
 
-  // TUNE SECTIONS
-  var sections = init_equaliser(".eq input");
+  // CHANGE HEADER ON SCROLLING
+  var equaliser = document.getElementById("equaliser");
+
+  $(window).scroll(function(){
+    if (equaliser != null) {
+      // move back to body if scroll top
+      var scrolltop = $(window).scrollTop();
+      if (scrolltop <= 49) {
+        var menu_equaliser = $("#equaliser");
+        $("#equaliser").detach();
+        menu_equaliser.appendTo("#equaliser_container");
+        $(".equaliser").css('color','white');
+        $(".equaliser").css('margin-top','5em');
+        $("#header").css('background','black');
+        $("#header").css('height','3rem');
+        $("body, .main_bg").css("background","black");
+      } else {
+
+        $.doTimeout( 'scrollTop', 50, function(){
+          // move equaliser to the menu if scroll down
+           var menu_equaliser = $("#equaliser");
+           $("#equaliser").detach();
+           $("#header").css('background','white');
+           $("#header").css('height','20em');
+           menu_equaliser.appendTo("#header");
+           $("#header .equaliser").css('color','black');
+           $("#header .equaliser").css('margin-top','-5em');
+           $("body, .main_bg").css("background","white");
+
+           // READ PREFERENCES
+           var sections = init_equaliser(".eq input");
+           var sorted_sections = []
+           for (var s in sections) { sorted_sections.push([s, sections[s]]); }
+           sorted_sections.sort(function(a, b) { return b[1] - a[1]; });
+
+          // LOAD SECTIONS ON SCROLL
+          // BUILD SECTIONS AT FIRST SCROLL
+          if (isScrolledIntoView('#maincontent') && !firstScroll) {
+              load_sections_first_time(sorted_sections);
+          }
+          // FOLLOWING TIMES
+          else if (isScrolledIntoView('#maincontent') && firstScroll) {
+            // update equaliser object
+            // if changed
+          } else {  };
+          });
+      }
+    }
+
+  });
+
+  // update equaliser on change
+  // $(".eq input").change(function() {
+  //   var iname = $(this).attr('id');
+  //   var num = $(this).val();
+  //   sections[iname] = num;
+  // });
+
+
+
 
 });
 
+function load_sections_first_time(sorted_sections) {
+    firstScroll = true;
+    for (i = 0; i < sorted_sections.length; i++) {
+      var section_id = sorted_sections[i][0]
+      var section_title = $("#"+section_id).next().text();
+      var sec = category_section(section_id,section_title,sorted_sections[i][1]);
+      $("#maincontent").append(sec);
+      // if section does not exist, create it
+      // if (document.getElementById("section_"+sorted_sections[i][0]) == null) {
+      //
+      // } else {
+      //   // otherwise update contents
+      //   document.getElementById("section_"+section_id+".counter").textContent=sorted_sections[i][1]
+      //   // and reorder
+      // }
+    }
+}
+
+function category_section(section_id, section_title, counter) {
+  var s = "<section class='row category_section justify-content-md-center' id='section_"+section_id+"'>\
+        <section class='row col-md-10 col-sm-10'>\
+          <h2 class='category_title col-md-3 col-sm-6'>"+section_title+"</h2>\
+          <p class='col-md-3 col-sm-6' id='section_"+section_id+"_counter' data-counter='"+counter+"'>"+counter+"</p>"
+  c = parseInt(counter)
+  if (c > 0) {
+    for (i = 0 ; i < parseInt(c/10); i++ ) {
+      s += "<p class='col-md-5 col-sm-6'>"+c[i]+"</p>"
+    }
+  }
+
+  s += "</section></section>"
+  return s
+}
 
 // callback function - query sonic index
 function call_index(selection) {
@@ -103,6 +158,7 @@ function call_index(selection) {
 
 }
 
+// create object section : value equaliser
 function init_equaliser(idx) {
   var sections = {};
   var inputs = $(idx);
@@ -114,9 +170,45 @@ function init_equaliser(idx) {
   return sections
 }
 
+// check if user has scrolled till a certain element
+function isScrolledIntoView(elem) {
+  var docViewTop = $(window).scrollTop();
+  var docViewBottom = docViewTop + $(window).height();
+  var elemTop = $(elem).offset().top;
+  var elemBottom = elemTop + $(elem).height();
+  return ((elemTop <= docViewBottom) && (elemTop >= docViewTop));
+}
+
 // show section on input change
 function showSection(num, name) {
+  // get an updated sections object
   var sections = init_equaliser(".eq input");
   sections[name] = num;
-  console.log(sections);
+
+  // create array to sort sections in descending order
+  var sorted_sections = []
+  for (var s in sections) { sorted_sections.push([s, sections[s]]); }
+  sorted_sections.sort(function(a, b) { return b[1] - a[1]; });
+
+  for (i = 0; i < sorted_sections.length; i++) {
+    var section_id = sorted_sections[i][0]
+    var section_title = $("#"+section_id).next().text();
+    // if section does not exist, create it
+    if (document.getElementById("section_"+sorted_sections[i][0]) == null) {
+      sec = category_section(section_id,section_title,sorted_sections[i][1]);
+      $("#maincontent").append(sec);
+    } else {
+      // update contents
+
+      $("#section_"+section_id+"_counter").attr('data-counter',sorted_sections[i][1]);
+      console.log($("#section_"+section_id+"_counter"));
+      // and reorder
+    }
+
+  }
+  // query config file for datasets in the category
+  // get the api for queries
+
+  // else create it
+  // return a number of results proportional to the value in the equaliser
 }
